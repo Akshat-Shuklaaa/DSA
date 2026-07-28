de66371fed71cfb257c88e83837ae2ef573527d7
Akshat.Shukla <161609011+Akshat-Shuklaaa@users.noreply.github.com>
2026-07-29 00:49:08 +0530

    Time: 5 ms (80.78%), Space: 46.2 MB (16.74%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,5 +1,16 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        
+
+if(x < 0)
+        return false;
+    long original = x;
+    long reverse = 0;
+    while(x!=0){
+        long digit = x % 10;
+        x /= 10;
+        reverse = reverse * 10 + digit;
     }
+    return original == reverse;
+}
+
 }
